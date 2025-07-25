596f2e10e9281147a2898f6fd9088dc8df11b1ca
Anthony Nwambu <aonwambu@gmail.com>
2025-07-25 23:31:44 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,11 @@
-import { useEffect } from 'react';
-import useRecipeStore from './store/useRecipeStore';
-import sampleRecipes from './data/sampleRecipes';
 import SearchBar from './components/SearchBar';
-import FilterDropdown from './components/FilterDropdown';
-import RecipeList from './components/RecipeList';
 
 function App() {
-  const setRecipes = useRecipeStore(state => state.setRecipes);
-
-  useEffect(() => {
-    setRecipes(sampleRecipes);
-  }, [setRecipes]);
-
   return (
-    <div className="max-w-4xl mx-auto px-4">
+    <div className="max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold text-center mt-4">Recipe Sharing App</h1>
       <SearchBar />
-      <FilterDropdown />
-      <RecipeList />
+      {/* Other components like FilterDropdown and RecipeList */}
     </div>
   );
 }
-
-export default App;
-
