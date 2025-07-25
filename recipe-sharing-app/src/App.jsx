769d5bae0872c278716532e555447bb2bdb568fd
Anthony Nwambu <aonwambu@gmail.com>
2025-07-25 15:22:25 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,27 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <div style={{ padding: '2rem', maxWidth: '600px', margin: '0 auto' }}>
-      <h1>🍳 Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <h1>
+        <Link to="/">🍳 Recipe Sharing App</Link>
+      </h1>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }
