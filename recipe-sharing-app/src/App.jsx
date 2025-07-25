afb0b9049427fe015ca9e67e9359bf5391e81229
Anthony Nwambu <aonwambu@gmail.com>
2025-07-25 23:14:33 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,13 @@
 import SearchBar from './components/SearchBar';
+import FilterDropdown from './components/FilterDropdown';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
-    <div>
+    <div className="max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold text-center mt-4">Recipe Sharing App</h1>
       <SearchBar />
+      <FilterDropdown />
       <RecipeList />
     </div>
   );
