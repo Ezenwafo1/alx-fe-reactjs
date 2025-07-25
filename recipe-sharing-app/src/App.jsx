eb88fb6808ecfe371c91d3659a52bd95586b0ff7
Anthony Nwambu <aonwambu@gmail.com>
2025-07-26 00:18:27 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,23 @@
+// src/App.jsx
+import React from 'react';
+import RecipeList from './components/RecipeList';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <div>
-      <FavoritesList />
-      <RecommendationsList />
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '1rem' }}>
+      <div>
+        <RecipeList />
+      </div>
+      <div>
+        <FavoritesList />
+      </div>
+      <div>
+        <RecommendationsList />
+      </div>
     </div>
   );
 }
+
+export default App;
