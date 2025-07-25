9dc0378485ba7c3f1400d0f2ac777af98a5728aa
Anthony Nwambu <aonwambu@gmail.com>
2025-07-26 00:18:57 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,24 @@
 // src/App.jsx
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '1rem' }}>
-      <div>
-        <RecipeList />
-      </div>
-      <div>
-        <FavoritesList />
-      </div>
-      <div>
-        <RecommendationsList />
-      </div>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">All Recipes</Link> |{' '}
+        <Link to="/favorites">Favorites</Link> |{' '}
+        <Link to="/recommendations">Recommendations</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/favorites" element={<FavoritesList />} />
+        <Route path="/recommendations" element={<RecommendationsList />} />
+      </Routes>
+    </Router>
   );
 }
 
