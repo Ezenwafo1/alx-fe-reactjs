66f7070faa63fe75b3a081e80975b49e060242bd
Anthony <aonwambu@gmail.com>
2025-08-08 21:46:03 +0100

    updated paragraph text to modify on different screen sizes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -11,7 +11,7 @@ function UserProfile() {
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
        <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 text-center">John Doe</h1>
       
-      <p className="text-gray-600 text-base">
+      <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
