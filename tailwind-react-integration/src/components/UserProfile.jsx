da7ea20c0ef690cd34023572af0a0ac876f4f5c4
Anthony Nwambu <aonwambu@gmail.com>
2025-09-12 15:59:10 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,7 @@
-function UserProfile() {
+function UserProfile() { 
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto rounded-lg shadow-lg text 
-    hover:shadow-xl">
-
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text hover:shadow-xl">
+      
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
@@ -10,7 +9,9 @@ function UserProfile() {
         hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       
-       <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500">John Doe</h1>
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500">
+        John Doe
+      </h1>
       
       <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
